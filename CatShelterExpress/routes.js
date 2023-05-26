a2f722ed26321c7312c26e7ca8d024d4b84ecbb5
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-05-26 14:53:02 +0300

    Add routes

---
+++ CatShelterExpress/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const homeController = require('./controllers/homeController');
+
+
+const router = express.Router();
+
+router.use('/',homeController);
+
+
+module.exports = router;
+
