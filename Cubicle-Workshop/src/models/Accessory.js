631beebaa2becd21c0ace64fe0bc8e0050fcf0fa
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-05-28 12:30:41 +0300

    Add Accessory.js

---
+++ Cubicle-Workshop/src/models/Accessory.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const accessorySchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    imageUrl:{
+        type:String,
+        required:true,
+        validate:{
+            validator:function(){
+               return this.imageUrl.startsWith('http');
+            },
+            message:'ImageUrl should be a link'
+        }
+    },
+    description:{
+        type:String,
+        required:true,
+        maxLength:120
+    },
+    cubes:[
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Cube'
+        }
+    ]
+});
+
+const Accessory = mongoose.model('Accessory', accessorySchema);
+
+module.exports = Accessory;
