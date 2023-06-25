98d826ce3eb86b16d753b1284dbb68a19050b058
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-06-25 10:04:59 +0300

    Add homeController

---
+++ Workshop Session and Authentication/src/controllers/homeController.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const cubeService = require('../services/cubeService');
+
+router.get('/', async (req, res) => {
+    const { search, from, to } = req.query;
+
+    const cubes = await cubeService.getAllCubes(search, from, to)
+    res.render('index', { cubes, search, from, to });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+module.exports = router;
