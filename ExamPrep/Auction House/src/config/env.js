c161c6efd63c6eb84a3341f5cfa945462d3ba915
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-07-20 09:01:24 +0300

    Add environments

---
+++ ExamPrep/Auction House/src/config/env.js
@@ -0,0 +1,5 @@
+exports.PORT = 3000;
+exports.CONNECTION_STRING = 'mongodb://localhost:27017/auctionHouse';
+exports.SECRET = 'fdsfndsnfssdgfdsb';
+exports.SALT_ROUNDS = 10;
+exports.SESSION_NAME = 'user';
