7e97ae1ea3dcc969449bdf4cd6bbc2211605fa51
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-08-08 09:57:57 +0300

    Add index.js

---
+++ ExamPrep/BookTalk/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { initializeDatabase } = require('./config/database');
+const { PORT } = require('./config/env');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+const { auth } = require('./middlewares/authMiddleware');
+
+const app = express();
+
+require('./config/handlebars')(app);
+
+app.use('/public', express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(auth);
+app.use(routes);
+
+initializeDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`App is listening on port ${PORT}...`));
+    })
+    .catch((err) => {
+        console.log('App cannot connect to DB', err);
+    });
