c612b89adbf1a48a1ec11d50fbd0d093f62a2f44
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-08-16 09:58:10 +0300

    Add environments

---
+++ ExamPrep/CryptoTrade/src/config/env.js
@@ -0,0 +1,5 @@
+exports.CONNECTION_STRING = 'mongodb://localhost:27017/cryptoTrade';
+exports.PORT = 3000;
+exports.SALT_ROUNDS = 10;
+exports.SECRET = 'fdsfdgijidfgolfsdfdsfdskm';
+exports.SESSION_NAME = 'user';
