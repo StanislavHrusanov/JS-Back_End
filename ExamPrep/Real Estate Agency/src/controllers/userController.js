5c30f0a45fd206a7091f04fd2b15cfe2d678d942
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-09-11 10:08:15 +0300

    Add userController

---
+++ ExamPrep/Real Estate Agency/src/controllers/userController.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const userService = require('../services/userService');
+const { validateUser } = require('../utils/validations');
+const { SESSION_NAME } = require('../config/env');
+const { isLoggedIn, isGuest } = require('../middlewares/authMiddleware')
+
+router.get('/register', isGuest, (req, res) => {
+    res.render('user/register');
+});
+
+router.post('/register', isGuest, async (req, res) => {
+    const userData = req.body;
+
+    try {
+        validateUser(userData);
+
+        const createdUser = await userService.register(userData);
+
+        const token = await userService.createToken(createdUser);
+
+        res.cookie(SESSION_NAME, token, { httpOnly: true });
+        res.redirect('/');
+
+    } catch (error) {
+        res.render('user/register', { userData, error });
+    }
+});
+
+router.get('/login', isGuest, (req, res) => {
+    res.render('user/login');
+});
+
+router.post('/login', isGuest, async (req, res) => {
+    const userData = req.body;
+
+    try {
+        const token = await userService.login(userData);
+
+        res.cookie(SESSION_NAME, token, { httpOnly: true });
+        res.redirect('/');
+
+    } catch (error) {
+        res.render('user/login', { userData, error });
+    }
+});
+
+router.get('/logout', isLoggedIn, (req, res) => {
+    res.clearCookie(SESSION_NAME);
+    res.redirect('/');
+});
+
+module.exports = router;
