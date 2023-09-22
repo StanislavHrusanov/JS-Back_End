405dcd5cea8e344d4be8637eb556a235f577d262
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-09-22 19:14:13 +0300

    Add index.js

---
+++ ExamPrep/Shared Trip/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { initializeDatabase } = require('./config/database');
+const { PORT } = require('./config/env');
+const cookieParser = require('cookie-parser');
+const { auth } = require('./middlewares/authMiddleware');
+const routes = require('./routes');
+
+const app = express();
+
+require('./config/handlebars')(app);
+
+app.use('/static', express.static('static'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(auth);
+app.use(routes);
+
+initializeDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`App is listening on port ${PORT}...`));
+    })
+    .catch((err) => {
+        console.log(`App cannot connect to database`, err);
+    });
+
+
